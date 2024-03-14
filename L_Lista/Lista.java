575feceba00eedb2c236ac35663c7b95f699208e
PaulSp46 <136258721+PaulSp46@users.noreply.github.com>
2024-03-14 12:26:07 +0000

    addSorted

--- L_Lista/Lista.java
+++ L_Lista/Lista.java
@@ -31,6 +31,31 @@ public class Lista {
         }
     }
 
+    public void addSorted(Nodo n){
+        if (isEmpty()) {
+            radice = n;
+            return;
+        } 
+
+        Nodo p1 = radice;
+        Nodo p2 = radice.getSuccessivo();
+        int vN = n.getValore();
+
+        if (p1.getValore()>vN) {
+            n.setSuccessivo(radice);
+            radice = n;
+            return;
+        }
+        
+        while (vN>p1.getValore() && p2!=null && vN>p2.getValore()) {
+            p1=p2;
+            p2=p1.getSuccessivo();
+        }
+        n.setSuccessivo(p2);
+        p1.setSuccessivo(n);
+    }
+    
+
     public String toString() {
         String s = "Elementi della lista: ";
         Nodo p = radice;
--- L_Lista/Main.java
+++ L_Lista/Main.java
@@ -9,9 +9,20 @@ public class Main {
         Lista l = new Lista();
         System.out.println( l );
 
-        for (int i=0; i<10; i++) {
+        /*for (int i=0; i<10; i++) {
             l.addTail( new Nodo( i, null ));
         }
+
+        l.addHead(new Nodo(2024, null));*/
+
+        l.addSorted(new Nodo(300, null));
+        l.addSorted(new Nodo(250, null));
+        l.addSorted(new Nodo(350, null));
+        l.addSorted(new Nodo(320, null));
+        l.addSorted(new Nodo(100, null));
+        l.addSorted(new Nodo(170, null));
+        l.addSorted(new Nodo(290, null));
+        l.addSorted(new Nodo(450, null));
 
         System.out.println( l );
     } 
